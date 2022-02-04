f8100804390909d19096ea56512d0cd54f62a2e5
unknown <mehrab.rahman@revature.com>
2022-02-04 10:19:42 -0800

    Adding buildtool and tests

---
+++ buildtool.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DESTINATION=/c/Users/mehrab/bin/pokeapp
+
+install() {
+	echo "Installing to $DESTINATION..."
+	cp pokeapp $DESTINATION
+}
+
+uninstall() {
+	echo "Uninstalling to $DESTINATION..."
+	rm "$DESTINATION"
+}
+
+test() {
+	echo "Testing..."
+	./pokeapp num Grass > numGrass.actual
+	cmp numGrass.actual numGrass.expected || echo "numGrass test failed..."
+}
+
+"$@"
+
